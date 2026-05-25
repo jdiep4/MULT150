86bbbd95d7c98cb1397d37014b66b3ef55e81b08
jdiep4 <22378715+jdiep4@users.noreply.github.com>
2026-05-24 23:08:12 -0700

    Used diff. UIs to create a menu

---
+++ jdiep_Hour14and16/Assets/Scripts/UserInterface.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserInterface : MonoBehaviour
+{
+    /*
+     *  I made the following modifications:
+     *  1. Changed the background image
+     *  2. Changed the text & text alignments
+     *  3. Changed the typeface
+     *  4. Changed the font
+     *  5. Created custom text specific to my game (game title and event description)
+     */
+}
